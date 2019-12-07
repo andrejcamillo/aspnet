76e0dfd203cf98ca628cd033edcef592c251604b
André <andrecamillo867@gmail.com>
2019-12-07 15:31:19 -0300

    aspnet

---
+++ Domain/Quarto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain
+{
+    [Table("Quartos")]
+    public class Quarto
+    {
+        [Key]
+        public int IdQuarto { get; set; }
+        public string NomeQuarto { get; set; }
+        public double PrecoQuarto { get; set; }
+        public Boolean Checado { get; set; }
+        public string EnderecoEntrega { get; set; }
+    }
+}
---
+++ EcommerceAngelo/wwwroot/js/cep.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $("#btnbuscarcep").click(function () {
+        cep = $("#Endend_Codigo").val();
+        $.getJSON("http://apps.widenet.com.br/busca-cep/api/cep/" + cep+".json", function (dados) {
+            $("#Telefone").val(cep.adress);
+        });
+    })
+
+
+
+})
